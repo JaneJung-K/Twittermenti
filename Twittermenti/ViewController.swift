8b395b8a1f9f34528302bfe3b2b0100eaf8f3849
JaneJung <piaojane@daum.net>
2021-03-25 23:58:29 +0900

    Updating the User Interface of Our App

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -23,58 +23,61 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        let prediction = try! setimentClassifier.prediction(text: "@Apple is the best company!")
-//
-//        print(prediction.label)
-        
-        swifter.searchTweet(using: "#blessed", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-            
-            var tweets = [TweetSentimentClassifierInput]()
-            
-            for i in 0..<100 {
-                if let tweet = results[i]["full_text"].string {
-                    let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
-                    tweets.append(tweetForClassification)
-                }
-            }
-            
-            do {
-                let predictions = try self.setimentClassifier.predictions(inputs: tweets)
-                
-                var sentimentScore = 0
-                
-                for pred in predictions {
-                    let sentiment = pred.label
-                    
-                    if sentiment == "Pos" {
-                        sentimentScore += 1
-                    } else if sentiment == "neg" {
-                        sentimentScore -= 1
-                    }
-                }
-                
-                print(sentimentScore)
-                
-            } catch {
-                print("There was an error with making a predition, \(error)")
-            }
-            
-            
-            
-            
-            
-            
-            
-        }) {(error) in
-            print("There was an error with the Twitter API Request, \(error)")
-        }
-
-        
     }
 
     @IBAction func predictPressed(_ sender: Any) {
     
+        if let searchText = textField.text {
+            
+            swifter.searchTweet(using: searchText, lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+                
+                var tweets = [TweetSentimentClassifierInput]()
+                
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetForClassification)
+                    }
+                }
+                
+                do {
+                    let predictions = try self.setimentClassifier.predictions(inputs: tweets)
+                    
+                    var sentimentScore = 0
+                    
+                    for pred in predictions {
+                        let sentiment = pred.label
+                        
+                        if sentiment == "Pos" {
+                            sentimentScore += 1
+                        } else if sentiment == "neg" {
+                            sentimentScore -= 1
+                        }
+                    }
+                    
+                    if sentimentScore > 20 {
+                        self.sentimentLabel.text = "😍"
+                    } else if sentimentScore > 10 {
+                        self.sentimentLabel.text = "😀"
+                    } else if sentimentScore > 0 {
+                        self.sentimentLabel.text = "🙂"
+                    } else if sentimentScore == 0 {
+                        self.sentimentLabel.text = "😐"
+                    } else if sentimentScore > -10 {
+                        self.sentimentLabel.text = "😕"
+                    } else if sentimentScore > -20 {
+                        self.sentimentLabel.text = "😡"
+                    } else {
+                        self.sentimentLabel.text = "🤮"
+                    }
+                    
+                } catch {
+                    print("There was an error with making a predition, \(error)")
+                }
+            }) {(error) in
+                print("There was an error with the Twitter API Request, \(error)")
+            }
+        }
     
     }
     
