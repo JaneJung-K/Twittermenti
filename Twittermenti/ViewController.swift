d16f93a69c836757fb02a048dd3cc3a68e69feaf
JaneJung <piaojane@daum.net>
2021-03-25 21:21:39 +0900

    sentimentClassifier 사용
    Neg, Pos 결과 정상적으로 출력 되는것 확인

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -8,25 +8,31 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
-    
-    let swifter = Swifter(consumerKey: "Au0iDkOssPkzgabDVV5vaLYso", consumerSecret: "MJGIXzOPtOOD9T9XpgNXwkMDzQRBQ1sGXC4SRjLX97bn71nMhx")
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let setimentClassifier = TweetSentimentClassifier()
+    
+    let swifter = Swifter(consumerKey: "Au0iDkOssPkzgabDVV5vaLYso", consumerSecret: "MJGIXzOPtOOD9T9XpgNXwkMDzQRBQ1sGXC4SRjLX97bn71nMhx")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let prediction = try! setimentClassifier.prediction(text: "@Apple is the best company!")
+        
+        print(prediction.label)
+        
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
-            print(results)
+            //print(results)
         }) {(error) in
             print("There was an error with the Twitter API Request, \(error)")
         }
 
-//        swifter.searchTweet(using: <#T##String#>, geocode: <#T##String?#>, lang: <#T##String?#>, locale: <#T##String?#>, resultType: <#T##String?#>, count: <#T##Int?#>, until: <#T##String?#>, sinceID: <#T##String?#>, maxID: <#T##String?#>, includeEntities: <#T##Bool?#>, callback: <#T##String?#>, tweetMode: <#T##TweetMode#>, success: <#T##Swifter.SearchResultHandler?##Swifter.SearchResultHandler?##(JSON, JSON) -> Void#>, failure: <#T##Swifter.FailureHandler##Swifter.FailureHandler##(Error) -> Void#>)
         
     }
 
